68a57226f128c151e429a422317b940a07840530
dhiiyaur <dhiyarifqi@gmail.com>
2022-05-25 05:42:50 +0700

    fix bug

--- pages/bookmark/index.jsx
+++ pages/bookmark/index.jsx
@@ -2,10 +2,22 @@ import Layout from '@/components/layout/Layout';
 import MangaCard from '@/components/card/MangaCard';
 import { useState, useEffect } from 'react';
 import BookmarkManager from '@/lib/store';
+import MangaDetailSkeleton from '@/components/loading/MangaDetailSkeleton';
+import { useRouter } from 'next/router';
+
 
 export default function BookmarkPage() {
+
+    let router = useRouter()
+
     const [mangaData, setMangaData] = useState([]);
     const [noData, setNoData] = useState(false)
+    const [isSkeletonLoading, setIsSkeletonLoading] = useState(false)
+
+    const handleSkeletonLoading = () => {
+        window.scrollTo(0, 0);
+        setIsSkeletonLoading(true)
+    }
 
     const initPage = () => {
         let mangaData = BookmarkManager.loadBookmark()
@@ -19,13 +31,19 @@ export default function BookmarkPage() {
         initPage()
     }, []);
 
+    useEffect(() => {
+        router.events.on("routeChangeStart", () => handleSkeletonLoading())
+    }, [router.events])
+
     return (
         <Layout>
-            <div className='grid grid-cols-2 sm:grid-cols-5 sm:gap-6 gap-4 px-5 py-2 mt-5'>
-                {mangaData?.map((value, index) => (
-                    <MangaCard value={value} source={value.Source} key={index} />
-                ))}
-            </div>
+            {isSkeletonLoading ? <MangaDetailSkeleton /> : (
+                <div className='grid grid-cols-2 sm:grid-cols-5 sm:gap-6 gap-4 px-5 py-2 mt-5'>
+                    {mangaData?.map((value, index) => (
+                        <MangaCard value={value} source={value.Source} key={index} />
+                    ))}
+                </div>
+            )}
         </Layout>
     )
 }
