2c519adf62f31433011ccb755da0e8b0d201e87b
dhiiyaur <dhiyarifqi@gmail.com>
2022-05-28 12:43:48 +0700

    fix bug, manga card

--- components/card/MangaCard.jsx
+++ components/card/MangaCard.jsx
@@ -3,18 +3,10 @@ import { useRouter } from 'next/router';
 export default function MangaCard({ value, source }) {
     let router = useRouter();
     return (
-        <div
-            className='w-full h-60 sm:h-72 rounded-xl cursor-pointer overflow-hidden bg-black'
+        <div className='h-60 sm:h-72 rounded-xl overflow-hidden'
             onClick={() => router.push(`/m/${source}/${value.Id}`)}
         >
-            <div>
-                <img
-                    src={value.Cover}
-                    // src={value.Covader}
-                    alt=''
-                    className='w-[99.75%]'
-                />
-            </div>
+            <img src={value.Cover} className='h-60 sm:h-72 object-fill w-full rounded-xl'></img>
             <div className='sticky inset-0 h-full w-full bg-gradient-to-t from-black'>
                 <div className='sticky top-[75%]'>
                     <div className='flex flex-col space-y-1.5 p-3 justify-end'>
@@ -37,6 +29,6 @@ export default function MangaCard({ value, source }) {
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
